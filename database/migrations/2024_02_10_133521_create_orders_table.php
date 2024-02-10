5849970c4af593546ae97a0902244afc0dfe391e
Abdullah-Omar-p <abdullahomarj1@gmail.com>
2024-02-10 17:00:50 +0200

    ..

--- database/migrations/2024_02_10_133521_create_orders_table.php
+++ database/migrations/2024_02_10_133521_create_orders_table.php
@@ -8,19 +8,16 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('total_price');
+            $table->tinyInteger('no_products');
             $table->timestamps();
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders');
     }
 };
--- database/migrations/2024_02_10_134113_create_debts_table.php
+++ database/migrations/2024_02_10_134113_create_debts_table.php
@@ -8,19 +8,18 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('debts', function (Blueprint $table) {
             $table->id();
+            $table->tinyInteger('status');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('liability');
+            $table->string('phone');
             $table->timestamps();
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('debts');
     }
 };
